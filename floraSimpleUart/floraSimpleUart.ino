10ce1bc673a5331258d9bd8e4d99c36de76b2b32
allen54a0 <allen54a0@gmail.com>
2018-09-26 00:42:10 +0800

    Create floraSimpleUart.ino

---
+++ floraSimpleUart/floraSimpleUart.ino
@@ -0,0 +1,193 @@
+/**
+   A BLE client for the Xiaomi Mi Plant Sensor, pushing measurements to an MQTT server.
+   
+   MIT License
+
+   Copyright (c) 2017 Sven Henkel
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
+
+#include "BLEDevice.h"
+
+
+#include "config.h"
+
+RTC_DATA_ATTR int bootCount = 0;
+
+// The remote service we wish to connect to.
+static BLEUUID serviceUUID("00001204-0000-1000-8000-00805f9b34fb");
+
+// The characteristic of the remote service we are interested in.
+static BLEUUID uuid_version_battery("00001a02-0000-1000-8000-00805f9b34fb");
+static BLEUUID uuid_sensor_data("00001a01-0000-1000-8000-00805f9b34fb");
+static BLEUUID uuid_write_mode("00001a00-0000-1000-8000-00805f9b34fb");
+
+static BLEAddress floraAddress(FLORA_ADDR);
+
+static int doConnect = 0;
+static boolean connected = false;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+
+
+
+bool getSensorData(BLEAddress pAddress, bool getBattery) {
+  Serial.print("Forming a connection to Flora device at ");
+  Serial.println(pAddress.toString().c_str());
+
+  BLEClient*  pClient  = BLEDevice::createClient();
+
+  // Connect to the remove BLE Server.
+  if (!pClient->connect(pAddress)) {
+      return false;
+  }
+  Serial.println(" - Connected to Flora");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    Serial.println(serviceUUID.toString().c_str());
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(uuid_write_mode);
+  uint8_t buf[2] = {0xA0, 0x1F};
+  pRemoteCharacteristic->writeValue(buf, 2, true);
+
+  delay(500);
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(uuid_sensor_data);
+  Serial.println(pRemoteService->toString().c_str());
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    Serial.println(uuid_sensor_data.toString().c_str());
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
+
+
+  // Read the value of the characteristic.
+  std::string value = pRemoteCharacteristic->readValue();
+  Serial.print("The characteristic value was: ");
+  const char *val = value.c_str();
+
+  Serial.print("Hex: ");
+  for (int i = 0; i < 16; i++) {
+    Serial.print((int)val[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
+
+  float temp = (val[0] + val[1] * 256) / ((float)10.0);
+  int moisture = val[7];
+  int light = val[3] + val[4] * 256;
+  int conductivity = val[8] + val[9] * 256;
+  
+  char buffer[64];
+  
+  Serial.print("Temperature: ");
+  Serial.println(temp);
+
+  
+
+  Serial.print("Moisture: ");
+  Serial.println(moisture);
+
+
+  Serial.print("Light: ");
+  Serial.println(light);
+
+
+  Serial.print("Conductivity: ");
+  Serial.println(conductivity);
+
+
+  if (getBattery) {
+    Serial.println("Trying to retrieve battery level...");
+    pRemoteCharacteristic = pRemoteService->getCharacteristic(uuid_version_battery);
+    if (pRemoteCharacteristic == nullptr) {
+      Serial.print("Failed to find our characteristic UUID: ");
+      Serial.println(uuid_sensor_data.toString().c_str()); 
+      return false;
+    }
+    Serial.println(" - Found our characteristic");
+
+    // Read the value of the characteristic...
+    value = pRemoteCharacteristic->readValue();
+    Serial.print("The characteristic value was: ");
+    const char *val2 = value.c_str();
+    Serial.print("Hex: ");
+    for (int i = 0; i < 16; i++) {
+      Serial.print((int)val2[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println(" ");
+
+    int battery = val2[0];
+    Serial.print("Battery: ");
+    Serial.println(battery);
+
+  }
+
+  pClient->disconnect();
+}
+
+void taskDeepSleep( void * parameter )
+{
+  delay(SLEEP_WAIT * 1000);
+  esp_sleep_enable_timer_wakeup(SLEEP_DURATION * 1000000ll);
+  Serial.println("Going to sleep now.");
+  esp_deep_sleep_start();
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  
+  xTaskCreate(      taskDeepSleep,          /* Task function. */
+                    "TaskDeepSleep",        /* String with name of task. */
+                    10000,                  /* Stack size in words. */
+                    NULL,                   /* Parameter passed as input of the task */
+                    1,                      /* Priority of the task. */
+                    NULL);                  /* Task handle. */
+  
+  
+  Serial.println("Starting Flora client...");
+  BLEDevice::init("");
+  
+  delay(1000);
+ 
+  getSensorData(floraAddress, ((bootCount % BATTERY_INTERVAL) == 0));
+  bootCount++;
+  
+  ////------------------LORA 連線&Retry 放這個 ----------------------------
+  Serial.println("Try to Send data via LoRa!!  TBD-----------------------XD "); 
+
+
+} // End of setup.
+
+
+// This is the Arduino main loop function.
+void loop() {
+  delay(10000);
+} // End of loop
